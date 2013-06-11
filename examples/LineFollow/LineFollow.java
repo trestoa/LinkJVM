938759171bb32078cd2afb8e15b9c8b4f6bf9484
Markus Klein <m@mklein.co.at>
2013-06-11 21:36:05 +0200

    added constants for servo and motor ports in linw-lollow example

--- examples/LineFollow/LineFollow.java
+++ examples/LineFollow/LineFollow.java
@@ -6,24 +6,30 @@
 import libkovan.*;
 
 public class LineFollow {
+
+	public static final int RIGHT_MOTOR_PORT = 0;
+	public static final int LEFT_MOTOR_PORT = 1;
+	public static final int RIGHT_SENSOR_PORT = 0;
+	public static final int LEFT_MOTOR_PORT = 1;
+
 	public static void main(String[] args){
 		Motor leftMotor = new Motor(0);
 		Motor rightMotor = new Motor(1);
 		Sensor leftSensor = new AnalogSensor(0);
 		Sensor rightSensor = new AnalogSensor(1);
 		while(true){
-		if(leftSensor.getValue() < 600){
-		leftMotor.drive(100);
-		rightMotor.drive(0);
-		}
-		else if(rightSensor.getValue() < 600){
-		leftMotor.drive(0);
-		rightMotor.drive(100);
-		}
-		else{
-		leftMotor.drive(100);
-		rightMotor.drive(100);
-		}
+			if(leftSensor.getValue() < 600){
+				leftMotor.drive(100);
+				rightMotor.drive(0);
+			}
+			else if(rightSensor.getValue() < 600){
+				leftMotor.drive(0);
+				rightMotor.drive(100);
+			}
+			else{
+				leftMotor.drive(100);
+				rightMotor.drive(100);
+			}
 		}
 	}
 }
