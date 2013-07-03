dd21604d45729afb8e879d3e0edda80d9fa07c2c
Markus Klein <m@mklein.co.at>
2013-07-03 06:21:54 +0200

    Added event interfaces for coming events

---
+++ src/java/libjvm/events/Event.java
@@ -0,0 +1,44 @@
+/*
+ * This file is part of LinkJVM.
+ *
+ * Java Framework for the KIPR Link
+ * Copyright (C) 2013 Markus Klein<m@mklein.co.at>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package libjvm.events;
+
+import java.util.ArrayList;
+
+/**
+ * 
+ * @author Markus Klein
+ *
+ */
+
+public interface Event {
+	public ArrayList<Action> actionList = new ArrayList<Action>();
+	
+	/**
+	 * This function is called permanently to proof if the event should be triggered or not.
+	 * 
+	 * @return true if the event should be triggered, false if not
+	 */
+	public boolean proof();
+	
+	
+	
+	
+}
