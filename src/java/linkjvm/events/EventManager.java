e89c7a8739bddf7ba753f9b3db73e91a331e263e
kleiinnn <m@mklein.co.at>
2013-08-17 18:32:30 +0200

    added event manager and basic structure of event manager

---
+++ src/java/linkjvm/events/EventManager.java
@@ -0,0 +1,29 @@
+import java.lang.ArrayList;
+
+public class EventManager extends Thread{
+	private ArrayList<Event> eventList;
+	
+	public EventManager(){
+		eventList = new ArrayList<Event>();
+	}
+
+	public void run(){
+		while(true){
+			Iterator<Event> it = eventList.iterator();
+			while(it.hasNext()){
+				Event nextEvent = it.next();
+				if(nextEvent.proof){
+					nextEvent.trigger();
+				}
+			}
+		}
+	}
+
+	public boolean registerEvent(Event e){
+		return eventList.add(e);
+	}
+
+	public boolean unregisterEvent(Event e){
+		return eventList.remove(e);
+	}
+}
