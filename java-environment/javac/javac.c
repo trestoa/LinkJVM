6363068523b0f5fee31620e93860af08eff5f50a
kleiinnn <m@mklein.co.at>
2013-09-01 16:27:04 +0200

    implemented javac wrapper

--- java-environment/javac/javac.c
+++ java-environment/javac/javac.c
@@ -1,2 +1,11 @@
-int main(){
+#include <stdio.h>
+#include <string.h>
 
+int main(int argc, char *argv[]){
+	char command[255] = "java /usr/local/LinkJVM/lib/ecj-3.7.jar ";
+	int i = 0;
+	for(i; i < argc; i++){
+		strcpy(command, argv[i]);
+	}
+	system(command);
+}
