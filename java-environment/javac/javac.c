3517fa9969ee7603b64bbec63aadc38d59b24761
kleiinnn <m@mklein.co.at>
2013-09-06 23:06:47 +0200

    fixed ecj jar location in javac wrapper

--- java-environment/javac/javac.c
+++ java-environment/javac/javac.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 int main(int argc, char *argv[]){
-	char command[255] = "java -jar /usr/local/LinkJVM/lib/ecj-3.7.jar ";
+	char command[255] = "java -jar /usr/local/LinkJVM/lib/javac/ecj-3.7.jar ";
 	int i = 1;
 	for(i; i < argc; i++){
 		strcat(command, argv[i]);
