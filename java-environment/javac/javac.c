350442752ed239e690f5e11b1656bd3011f54fa1
kleiinnn <m@mklein.co.at>
2013-10-27 23:06:41 +0100

    finally got the right ecj location

--- java-environment/javac/javac.c
+++ java-environment/javac/javac.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 int main(int argc, char *argv[]){
-	char command[2048] = "java -jar /usr/ecj/ecj-3.7.jar -7 ";
+	char command[2048] = "java -jar /usr/lib/ecj/ecj-3.7.jar -7 ";
 	int i = 1;
 	for(i; i < argc; i++){
 		strcat(command, argv[i]);
