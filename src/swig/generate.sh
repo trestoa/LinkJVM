461f349ad2c53c58d1005f1b08827155ad4eec55
kleiinnn <m@mklein.co.at>
2013-11-03 12:35:43 +0100

    delete old files before generating new files

--- src/swig/generate.sh
+++ src/swig/generate.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
+rm *.cxx
+rm ../java/io/github/linkjvm/jni/*.java
 for f in *.i; do
 	dirname=`echo "$f" | cut -d'.' -f1`
 	if [ ! -d "../java/io/github/linkjvm/jni/$dirname" ]; then
 		swig -c++ -package io.github.linkjvm.jni -outdir ../java/io/github/linkjvm/jni -java $f
 	else
+		rm ../java/io/github/linkjvm/jni/$dirname/*.java
 		swig -c++ -package io.github.linkjvm.jni.$dirname -outdir ../java/io/github/linkjvm/jni/$dirname -java $f
 	fi
 done
