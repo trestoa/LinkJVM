fdb8b5cb06c0a284fedbe8e4dfd915037506ad68
kleiinnn <m@mklein.co.at>
2013-11-03 11:42:48 +0100

    fixed directory bugs

--- src/swig/generate.sh
+++ src/swig/generate.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 for f in *.i; do
 	dirname=`echo "$f" | cut -d'.' -f1`
-	if [ ! -d "../java/io/github/linkjvm/$dirname" ]; then
+	if [ ! -d "../java/io/github/linkjvm/jni/$dirname" ]; then
 		swig -c++ -package io.github.linkjvm.jni -outdir ../java/io/github/linkjvm/jni -java $f
 	else
 		swig -c++ -package io.github.linkjvm.jni.$dirname -outdir ../java/io/github/linkjvm/jni/$dirname -java $f
