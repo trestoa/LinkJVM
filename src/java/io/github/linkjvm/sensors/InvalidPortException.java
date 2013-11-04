750c9fc510c1b26008ae59da4fddd34c70e9c1d5
kleiinnn <m@mklein.co.at>
2013-11-04 20:50:21 +0100

    added InvalidPortException to avoid wrong port on sensor instantiation

---
+++ src/java/io/github/linkjvm/sensors/InvalidPortException.java
@@ -0,0 +1,13 @@
+package io.github.linkjvm.sensors;
+
+/**
+ *
+ */
+public class InvalidPortException extends Exception{
+	public InvalidPortException(){
+		super();
+	}
+	public InvalidPortException(String message){
+		super(message);
+	}
+}
