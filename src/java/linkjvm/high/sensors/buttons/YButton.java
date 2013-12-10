ebefc4db122f79d653e125267ff289aa44ee35dd
kleiinnn <m@mklein.co.at>
2013-12-10 21:10:06 +0100

    added button classes and filled classes with with stubs

---
+++ src/java/linkjvm/high/sensors/buttons/YButton.java
@@ -0,0 +1,17 @@
+package linkjvm.high.sensors.buttons;
+
+public class YButton extends AbstractButton{
+
+	@Override
+	public boolean isPressed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean getValue() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
