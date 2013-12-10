e2abc0f1c2d01f178600d8a4e65c0c0aa620deb3
kleiinnn <m@mklein.co.at>
2013-12-10 21:35:21 +0100

    removed jniSensor

--- src/java/linkjvm/high/sensors/buttons/AButton.java
+++ src/java/linkjvm/high/sensors/buttons/AButton.java
@@ -1,7 +1,11 @@
 package linkjvm.high.sensors.buttons;
 
 public class AButton extends AbstractButton{
-
+	
+	public AButton(){
+		
+	}
+	
 	@Override
 	public boolean isPressed() {
 		// TODO Auto-generated method stub
