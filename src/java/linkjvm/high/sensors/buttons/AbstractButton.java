41260c9a04123698643cd01a5e473132e08d5005
kleiinnn <m@mklein.co.at>
2013-12-10 21:40:29 +0100

    removed jniSensor (reverted from commit e2abc0f1c2d01f178600d8a4e65c0c0aa620deb3)

--- src/java/linkjvm/high/sensors/buttons/AbstractButton.java
+++ src/java/linkjvm/high/sensors/buttons/AbstractButton.java
@@ -1,10 +1,8 @@
 package linkjvm.high.sensors.buttons;
 
 import linkjvm.high.sensors.digital.AbstractDigitalSensor;
-import linkjvm.low.buttons.IdButton;
 
-public abstract class AbstractButton implements AbstractDigitalSensor{
-	private IdButton jniButton;
+public abstract class AbstractButton implements AbstractDigitalSensor{	
 	
 	/**
 	 * 
