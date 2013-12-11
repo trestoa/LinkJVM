cb86495fb81fd0fe59880dd790c969b775b5a770
Markus Klein <m@mklein.co.at>
2013-12-11 15:59:14 +0100

    added interface for text button

---
+++ src/java/linkjvm/high/sensors/buttons/TextButton.java
@@ -0,0 +1,16 @@
+package linkjvm.high.sensors.buttons;
+
+public interface TextButton {
+	
+	/**
+	 * 
+	 * @param text
+	 */
+	public void setText(String text);
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getText();
+}
