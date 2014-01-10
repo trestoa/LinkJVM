21f47c1a25215d804652dca1e6fa01e5564cc4b9
kleiinnn <m@mklein.co.at>
2014-01-10 20:26:33 +0100

    implemented camera config

---
+++ src/java/linkjvm/high/vision/CameraConfig.java
@@ -0,0 +1,102 @@
+package linkjvm.high.vision;
+
+import linkjvm.low.factory.JNIController;
+import linkjvm.low.vision.Config;
+import linkjvm.low.vision.InputProvider;
+
+public class CameraConfig {
+	
+	/**
+	 * 
+	 */
+	public static final String DEFAULT_CHANNEL_CONFIG_NAME = "default";
+	
+	private final Resolution res;
+	private final InputProvider inputProvider;
+	private final Config channelConfig;
+	
+	/**
+	 * 
+	 * @param res
+	 * @param inputProvider
+	 */
+	public CameraConfig(Resolution res, InputProvider inputProvider, String channelConfigName) {
+		super();
+		this.res = res;
+		this.inputProvider = inputProvider;
+		this.channelConfig = JNIController.getInstance().getConfigFactory().getInstance(channelConfigName);
+	}
+	
+	/**
+	 * 
+	 * @param inputProvider
+	 */
+	public CameraConfig(InputProvider inputProvider, String channelConfigName) {
+		this(Resolution.MED_RES, inputProvider, channelConfigName);
+	}
+	
+	/**
+	 * 
+	 * @param res
+	 */
+	public CameraConfig(Resolution res, String channelConfigName){
+		this(res, JNIController.getInstance().getUsbInputProviderFactory().getInstance(), channelConfigName);
+	}
+	
+	/**
+	 * 
+	 * @param res
+	 */
+	public CameraConfig(Resolution res){
+		this(res, JNIController.getInstance().getUsbInputProviderFactory().getInstance(), DEFAULT_CHANNEL_CONFIG_NAME);
+	}
+	
+	/**
+	 * 
+	 * @param inputProvider
+	 */
+	public CameraConfig(InputProvider inputProvider){
+		this(Resolution.MED_RES, inputProvider, DEFAULT_CHANNEL_CONFIG_NAME);
+	}
+	
+	/**
+	 * 
+	 * @param channelConfigName
+	 */
+	public CameraConfig(String channelConfigName){
+		this(Resolution.MED_RES, JNIController.getInstance().getUsbInputProviderFactory().getInstance(), channelConfigName);
+	}
+	
+	/**
+	 * 
+	 */
+	public CameraConfig(){
+		this(Resolution.MED_RES, JNIController.getInstance().getUsbInputProviderFactory().getInstance(), DEFAULT_CHANNEL_CONFIG_NAME);
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public Resolution getRes() {
+		return res;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public InputProvider getInputProvider() {
+		return inputProvider;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public Config getChannelConfig(){
+		return channelConfig;
+	}
+	
+	
+}
