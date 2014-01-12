4aaa5aa6962ea7cdae95fd78ba60140b8308e92a
kleiinnn <m@mklein.co.at>
2014-01-12 21:42:11 +0100

    documentation

--- src/java/linkjvm/high/debugger/Debugger.java
+++ src/java/linkjvm/high/debugger/Debugger.java
@@ -6,9 +6,11 @@ import java.net.InetSocketAddress;
 import java.net.Socket;
 
 /**
- * 
+ * The LinkJVM-Debugger is used to send bug information to a LinkJVM-Debugger-Client 
+ * which receives the message in displays it.
  * @author Markus Klein
- *
+ * @version 2.0.0
+ * @since 2.0.0
  */
 public class Debugger {	
 	public static final int DEFAULT_PORT = 62512;
@@ -17,24 +19,34 @@ public class Debugger {
 	private PrintWriter writer = null;
 	
 	/**
-	 * 
+	 * Constructs a new debugger.
 	 */
 	public Debugger(){
 		socket = new Socket();
 	}
 	
 	/**
-	 * 
-	 * @param host
+	 * Construct a new debugger and establish a connection to the given host.
+	 * The invocation is the same as:
+	 * <code>
+	 * Debugger debugger = new Debugger();
+	 * debugger.connect(host);
+	 * </code>
+	 * @param host IP-address of the debugger client.
 	 */
 	public Debugger(String host){
 		this(host, DEFAULT_PORT);
 	}
 	
 	/**
-	 * 
-	 * @param host
-	 * @param port
+	 * Construct a new debugger and establish a connection to the given host and TCP port.
+	 * The invocation is the same as:
+	 * <code>
+	 * Debugger debugger = new Debugger();
+	 * debugger.connect(host, port);
+	 * </code>
+	 * @param host IP-address of the debugger client
+	 * @param port TCP where the debugger client run
 	 */
 	public Debugger(String host, int port){
 		socket = new Socket();
@@ -42,19 +54,19 @@ public class Debugger {
 	}
 	
 	/**
-	 * 
-	 * @param host
-	 * @return
+	 * Establish a connection to the given host.
+	 * @param host IP-address of the debugger client.
+	 * @return <code>true</code> on success and <code>false</code> on failure.
 	 */
 	public boolean connect(String host){
 		return connect(host);
 	}
 	
 	/**
-	 * 
-	 * @param host
-	 * @param port
-	 * @return
+	 * Establish a connection to the given host and TCP port.
+	 * @param host IP-address of the debugger client
+	 * @param port TCP where the debugger client run
+	 * @return <code>true</code> on success and <code>false</code> on failure.
 	 */
 	public boolean connect(String host, int port){
 		try {
@@ -68,7 +80,7 @@ public class Debugger {
 	}
 	
 	/**
-	 * 
+	 * Closes the connection to the client.
 	 */
 	public void close(){
 		try {
@@ -81,16 +93,16 @@ public class Debugger {
 	}
 	
 	/**
-	 * 
-	 * @param text
+	 * Sends a debug message to the client and adds a new-line at the end of the message.
+	 * @param text message to send
 	 */
 	public boolean writeln(String text){
 		return write(text + "\n");
 	}
 	
 	/**
-	 * 
-	 * @param text
+	 * Sends a debug message to the client.
+	 * @param text message to send
 	 */
 	public boolean write(String text){
 		if(!socket.isClosed() && writer != null){
